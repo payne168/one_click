5289fdd483072dd8b16d2063ebfb16bd835bb6e3
payne <leipayne18@gmail.com>
2020-09-01 10:24:41 +0800

    script调试

--- one_click.sh
+++ one_click.sh
@@ -1,2 +1,10 @@
-#!$PERFIX/bin/bash
+#!$PREFIX/bin/bash
 apt-get update -y
+#然后是安装需要的软件
+apt install openssh
+apt install python
+apt install git
+pkg install termux-auth
+pkg install termux-tools
+pkg install clang
+pkg install libxml2 libxslt libiconv
